91c904ceade1ff192403d04a1406ddd41a675f7a
gv <gvdevops00@gmail.com>
2022-07-17 21:43:57 +0530

    auto pushing chainges repo

--- components/redis.sh
+++ components/redis.sh
@@ -1,7 +1,6 @@
 curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
 yum install redis-6.2.7 -y
 
-sudo -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
-
+sudo -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
 systemctl enable redis
 systemctl start redis
