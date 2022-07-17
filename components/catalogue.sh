c665231e60452a16b8daa21aeb07445aa8fe5c40
gv <gvdevops00@gmail.com>
2022-07-17 18:59:18 +0530

    auto pushing chainges repo

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -11,7 +11,7 @@ mv catalogue-main catalogue
 cd /home/roboshop/catalogue
 npm install
 
-sed -i -e 's/MONGO_DNSNAME/mongodb.devopsb656.online/' /home/roboshop/catalogue/systemd.service
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service
 
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
 systemctl daemon-reload
