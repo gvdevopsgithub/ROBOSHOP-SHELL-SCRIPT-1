74d954e4476ca6bc0eded3e50f3a718c0468acac
gv <gvdevops00@gmail.com>
2022-07-18 11:56:08 +0530

    auto pushing chainges repo

--- components/cart.sh
+++ components/cart.sh
@@ -1,6 +1,5 @@
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
-
 useradd roboshop
 curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
 cd /home/roboshop
@@ -9,7 +8,6 @@ unzip /tmp/cart.zip
 mv cart-main cart
 cd cart
 npm install
-
 set -i -e 's/REDIS_ENDPOINT/redis.devopsb656.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.devopsb656.online/' /home/roboshop/cart/systemd.service
 mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
 systemctl daemon-reload
