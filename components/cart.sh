b4801226eb351d5c9b97f1046df1cd12d8c07a5f
gv <gvdevops00@gmail.com>
2022-08-01 15:51:51 +0530

    auto pushing chainges repo

--- components/cart.sh
+++ components/cart.sh
@@ -11,7 +11,10 @@ yum install nodejs -y &>>${LOG}
 CHECK_STAT $?
 
 PRINT "Creating Application User"
-useradd roboshop &>>${LOG}
+id roboshop &>>${LOG}
+if [ $? -ne 0 ]; then
+  useradd roboshop &>>${LOG}
+fi
 CHECK_STAT $?
 
 PRINT "Downloading Cart Content"
@@ -31,7 +34,7 @@ CHECK_STAT $?
 mv cart-main cart
 cd cart
 
-PRINTecho "Install NodeJS Dependencies for Cart Content"
+PRINT "Install NodeJS Dependencies for Cart Content"
 npm install &>>${LOG}
 CHECK_STAT $?
 
