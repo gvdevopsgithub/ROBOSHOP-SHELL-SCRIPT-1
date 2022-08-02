3b8a7aa7a22257e70e54f99ce74d8c641a156bb1
gv <gvdevops00@gmail.com>
2022-08-02 15:51:50 +0530

    auto pushing chainges repo

--- components/common.sh
+++ components/common.sh
@@ -49,7 +49,7 @@ APP_COMMON_SETUP() {
 }
 
 SYSTEMD() {
-    PRINT "Update SystemD Configuration"
+  PRINT "Update SystemD Configuration"
     set -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/USERHOST/users.roboshop.internal/' -e 's/AMQPHOST/rabbitmq.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>${LOG}
     CHECK_STAT $?
 
