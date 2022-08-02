fad33b6f2597c3c1cfe94f27adb761824e3b9118
gv <gvdevops00@gmail.com>
2022-08-02 19:32:26 +0530

    auto pushing chainges repo

--- components/redis.sh
+++ components/redis.sh
@@ -11,7 +11,7 @@ yum install redis-6.2.7 -y &>>${LOG}
 CHECK_STAT $?
 
 PRINT "Configure Redis Config"
-sudo -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${LOG}
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${LOG}
 CHECK_STAT $?
 
 PRINT "Start Redis"
