3e8b21e0817b9bf44ae25a32ee47380411ae5610
gv <gvdevops00@gmail.com>
2022-08-02 18:45:33 +0530

    auto pushing chainges repo

--- components/mysql.sh
+++ components/mysql.sh
@@ -2,17 +2,41 @@ source components/common.sh
 
 CHECK_ROOT
 
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
-yum install mysql-community-server -y
-systemctl enable mysqld
-systemctl start mysqld
+if [ -z "${MYSQL_PASSWORD}" ]; then
+  echo "Need MYSQL_PASSWORD env variable"
+  exit 1
+fi
+
+PRINT "Configure YUM Repos"
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>${LOG}
+CHECK_STAT $?
+
+PRINT "Install MYSQL"
+yum install mysql-community-server -y &>>${LOG}
+systemctl enable mysqld &>>${LOG} && systemctl start mysqld &>>${LOG}
+CHECK_STAT $?
+
 
 MYSQL_DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql --connect-expired-password -uroot -p"${MYSQL_DEFAULT_PASSWORD}"
-echo "uninstall plugin validate_password;" | mysql -uroot -p"${MYSQL_PASSWORD}"
-curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
-cd /tmp
-unzip mysql.zip
-cd mysql-main
-mysql -u root -p"${MYSQL_PASSWORD}" <shipping.sql
+echo show databases | mysql -uroot -p"${MYSQL_PASSWORD}" &>>${LOG}
+if [ $? -ne 0 ]; then
+  PRINT "RESET ROOT PASSWORD"
+  echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql --connect-expired-password -uroot -p"${MYSQL_DEFAULT_PASSWORD}" &>>${LOG}
+  CHECK_STAT $?
+fi
+
+echo show plugins | mysql -uroot -p"${MYSQL_PASSWORD}" 2>>${LOG} | grep validate_password &>>${LOG}
+if [ $? -eq 0 ]; then
+  PRINT "Uninstall Password Validate Plugin"
+  echo "uninstall plugin validate_password;" | mysql -uroot -p"${MYSQL_PASSWORD}" &>>${LOG}
+  CHECK_STAT $?
+fi
+
+PRINT "Download Schema"
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>${LOG}
+CHECK_STAT $?
+
+PRINT "Load Schema"
+cd /tmp && unzip mysql.zip &>>${LOG} && cd mysql-main && mysql -u root -p"${MYSQL_PASSWORD}" <shipping.sql &>>${LOG}
+CHECK_STAT $?
